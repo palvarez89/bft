632e945631aad5c9ecf945e99224d0179c890cc0
Pedro Alvarez <pedro.alvarez@codethink.co.uk>
2020-03-28 16:22:22 +0000

    bft_types: Add Instruction and Program types

--- bft_types/src/lib.rs
+++ bft_types/src/lib.rs
@@ -1,2 +1,22 @@
+use std::path::PathBuf;
+
+#[derive(Debug)]
+enum Instruction {
+    MoveLeft,
+    MoveRight,
+    Increment,
+    Decrement,
+    Input,
+    Output,
+    BeginLoop,
+    EndLoop,
+}
+
+#[derive(Debug)]
+struct Program {
+    filename: PathBuf,
+    instructions: Vec<Instruction>,
+}
+
 #[cfg(test)]
 mod tests {}
