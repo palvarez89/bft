d8c6d1b45f2f25ecb1c62444810f6d5e86cec314
Pedro Alvarez <pedro.alvarez@codethink.co.uk>
2020-03-28 17:11:59 +0000

    Use Program from main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use bft_types;
+use std::env::args;
+use std::path::Path;
+
+fn main() -> Result<(), Box<dyn std::error::Error + 'static>> {
+    let fname = args().nth(1).ok_or("Expected filename")?;
+
+    let the_program = bft_types::Program::from_file(Path::new(&fname));
+    println!("{:?}", the_program);
+    Ok(())
 }
