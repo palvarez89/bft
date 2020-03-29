a18ee279c6e57373d130fb8d0232ee1f0434ba21
Pedro Alvarez <pedro.alvarez@codethink.co.uk>
2020-03-29 23:12:18 +0100

    Load program from main

--- src/main.rs
+++ src/main.rs
@@ -6,9 +6,8 @@ use std::path::Path;
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let fname = args().nth(1).ok_or("Expected filename")?;
 
-    let the_program = bft_types::Program::from_file(Path::new(&fname));
+    let the_program = bft_types::Program::from_file(Path::new(&fname))?;
     let memory = bft_interp::VirtualMachine::<u8>::new(0, false);
-    println!("{:?}", the_program);
-    println!("{:?}", memory);
+    memory.load_program(&the_program);
     Ok(())
 }
